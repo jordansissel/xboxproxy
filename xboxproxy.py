2adce3b7410076ffd3cbf37c9252638e69f08c38
Jordan Sissel <jls@semicomplete.com>
2010-12-31 14:52:44 -0800

    - more hackery

--- xboxproxy.py
+++ xboxproxy.py
@@ -28,7 +28,6 @@ class XboxProxy(object):
   #   If ether dst is broadcast, broadcast
   #   Else, look up target in cam.
   def packet(self, p):
-    #print repr(p)
     if p[IP].src == "0.0.0.1":
       location = "local"
     else:
@@ -43,20 +42,17 @@ class XboxProxy(object):
     identity = p[Ether].src
     self.add_to_cam(identity, location)
 
-
-    # Push the packet.
     ether_dest = p[Ether].dst
     if ether_dest == "ff:ff:ff:ff:ff:ff":
+      print "%s(%s): %d bytes (broadcast)" % (location, identity, len(p))
       if self.default_broadcast:
         self.sendto(p, self.default_broadcast)
       else:
-        print "%s(%s): %d bytes (broadcast)" % (location, identity, len(p))
         locs = [location]
         for cam_id, cam_loc in self.cam_table.iteritems():
-          if cam_loc in locs:
-            continue
-          locs.append(cam_loc)
-          self.sendto(p, cam_loc)
+          if cam_loc not in locs:
+            locs.append(cam_loc)
+            self.sendto(p, cam_loc)
 
     else:
       # look up destination mac in cam table
@@ -71,7 +67,7 @@ class XboxProxy(object):
   # def packet
 
   def sendto(self, p, destination):
-    print "Sending to %s: %r" % (destination, p)
+    #print "Sending to %s: %r" % (destination, p)
     if destination == "local":
       sendp(p)
     else:
